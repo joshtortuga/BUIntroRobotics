6fa70ba870acf530252ee526b97334237a0db22d
joshtortuga <joshua.tortuga0119@gmail.com>
2021-03-21 14:11:31 -0500

    Add files via upload

--- HW3/Number2.m
+++ HW3/Number2.m
@@ -1,4 +1,6 @@
 clear all; close all; clc;
 
 T = transl(0,.5,0)*trotz(pi/3)*transl(1,0,0);
+title("3D Plot")
 trplot(T)
+set(gcf, 'Name', '3D Plot')
--- HW3/Number5.m
+++ HW3/Number5.m
@@ -1,17 +1,27 @@
 clc; clear; close all;
 
+%loads the given data
 load  HW3_traj.mat
 mdl_puma560
+
+%Figure that shows the plot trajetory
 figure(1)
 plot3(traj(:,1),traj(:,2),traj(:,3)),grid;
 title("Plot Trajectory")
+set(gcf, 'Name', 'Plot Trajectory')
 Tp = SE3(0.6,0,0) * SE3(traj) * SE3.oa([0 1 0], [0 0 -1]);
 q = p560.ikine6s(Tp);
+
+%Figure that shows the animation of the robot moving
 figure(2)
 title("Animation")
+set(gcf, 'Name', 'Robot Animation')
 p560.plot(q)
 TE = p560.fkine(q);
 p = TE.transl;
+
+%figure that shows the trajectory of the robot using plot p
 figure(3)
-title("Plot P")
 plot3(p(:,1),p(:,2),p(:,3)), grid;
+title("Plot P")
+set(gcf, 'Name', 'Plot P Trajectory')
