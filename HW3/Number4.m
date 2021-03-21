3d6a96fd60f80c69cd5606cdcf087e032cd56bdb
joshtortuga <joshua.tortuga0119@gmail.com>
2021-03-21 14:10:53 -0500

    Add files via upload

--- HW3/Number4.m
+++ HW3/Number4.m
@@ -5,5 +5,7 @@ T1 = [.5 -.866 0 .5; 0.866 .5  0 .1; 0 0 1 0; 0 0 0 1];
 T2 = [.866 -.5 0 .5; .5 .866 0 -.1; 0 0 1 0; 0 0 0 1];
 t = [0:0.1:2]';
 
+title("Trajectory")
 q = p560.jtraj(T1,T2,t);
-qplot(t,q);
+qplot(t,q)
+set(gcf, 'Name', 'Trajectory')
