ba20702ceabc4a3578eb5a4ab62a75e38a588e79
joshtortuga <joshua.tortuga0119@gmail.com>
2021-03-21 14:18:05 -0500

    Add files via upload

--- HW3/Number2.m
+++ HW3/Number2.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-T = transl(0,.5,0)*trotz(pi/3)*transl(1,0,0);
+Tranlation = transl(0,.5,0)*trotz(pi/3)*transl(1,0,0);
 title("3D Plot")
-trplot(T)
+trplot(Translation)
 set(gcf, 'Name', '3D Plot')
